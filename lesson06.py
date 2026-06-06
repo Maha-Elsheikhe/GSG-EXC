68569063a90b11f86968e679f3b963e6b834a920
Maha Elsheikhe <mahaelshakhe@gmail.com>
2026-06-06 11:43:38 +0300

    Edit lesson06 file

--- lesson06.py
+++ lesson06.py
@@ -1,30 +1,30 @@
 # Exercise 1
-# num1 = float(input(f"Enter the first number:"))
-# num2 = float(input(f"Enter the second number:"))
+num1 = float(input(f"Enter the first number:"))
+num2 = float(input(f"Enter the second number:"))
 
-# print(f"Sum => {num1 + num2}")
-# print(f"Subtract => {num1 - num2}")
-# print(f"Multiply => {num1 * num2}")
-# print(f"Divide => {num1 / num2}")
-# print(f"Floor Divide => {num1 // num2}")
-# print(f"Modulo => {num1 % num2}")
-# print(f"Power => {num1 ** num2}")
+print(f"Sum => {num1 + num2}")
+print(f"Subtract => {num1 - num2}")
+print(f"Multiply => {num1 * num2}")
+print(f"Divide => {num1 / num2}")
+print(f"Floor Divide => {num1 // num2}")
+print(f"Modulo => {num1 % num2}")
+print(f"Power => {num1 ** num2}")
 
 # Exercise 2
-# text = input("Enter your text statement:")
+text = input("Enter your text statement:")
 
-# print(text.upper())
-# print(len(text))
-# print(text.split(" "))
-# print("Python"in text)
-# print(text[::-1])
+print(text.upper())
+print(len(text))
+print(text.split(" "))
+print("Python"in text)
+print(text[::-1])
 
 # Exercise 3
-# number = "3.7"
+number = "3.7"
 
-# print(f"Convert String To Float => {float(number)}, Type => Float")
-# print(f"Convert Float To Int => {int(float(number))}, Type => Int")
-# print(f"Convert Int To String => {str(number)}, Type => String")
+print(f"Convert String To Float => {float(number)}, Type => Float")
+print(f"Convert Float To Int => {int(float(number))}, Type => Int")
+print(f"Convert Int To String => {str(number)}, Type => String")
 
 # Exercise 4
 password = input("Enter Your Password:")
